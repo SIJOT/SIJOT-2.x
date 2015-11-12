979d0d1341a684a4fe0f636a26fbf669da115c1a
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-11-12 09:21:49 +0100

    Update platform.php

--- config/platform.php
+++ config/platform.php
@@ -8,8 +8,17 @@
  | This is the platform configuration. Below is a short description of the config variables. 
  | The most important config variables are stored in the .env. Be sure these are also correct.
  | 
+ | config/platform.php 
+ | =====================
+ | [webmasterEmail] =
+ | [webmasterName]  =
  */ 
 
 
 return [
+ /**
+  * Webmaster details
+  */
+ 'webmasterEmail' => 'Topairy@gmail.com', 
+ 'webmasterName'  => 'Tim Joosten'
 ];
