53cc23e41986574689ff0f31e796565cdffa1766
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-11-12 09:26:19 +0100

    Update platform.php

--- config/platform.php
+++ config/platform.php
@@ -13,6 +13,7 @@
  | [webmasterEmail] = The email addrees of the webmaster
  | [webmasterName]  = The Name off the webmaster.
  | [websiteName]    = The name of the website. This is used in the navbar and page title.
+ | [websiteContact] = The contact info email address off the scouting group.
  */ 
 
 
@@ -20,11 +21,12 @@ return [
  /**
   * Webmaster details
   */
- 'webmasterEmail' => 'Topairy@gmail.com', 
- 'webmasterName'  => 'Tim Joosten',
+ 'webmasterEmail'  => 'Topairy@gmail.com', 
+ 'webmasterName'   => 'Tim Joosten',
  
  /**
   * Website Info.
   */
-  'websiteName'   => 'Sint-Joris'
+  'websiteName'    => 'Sint-Joris', 
+  'websiteContact' => 'Contact@qst-joris-turnhout.be',
 ];
