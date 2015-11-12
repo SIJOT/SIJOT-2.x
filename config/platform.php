7b666bc4cffc2b467c4900fda87f2a772cd3ec86
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-11-12 09:18:01 +0100

    Create platform.php

---
+++ config/platform.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* 
+ |-------------------------------------------------------------------------- 
+ | Platform config
+ |-------------------------------------------------------------------------- 
+ | 
+ | This is the platform configuration. Below is a short description of the config variables. 
+ | The most important config variables are stored in the .env. Be sure these are also correct.
+ | 
+ */ 
+
+
+return [
+];
