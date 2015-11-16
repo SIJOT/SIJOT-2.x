dbb930a28262df368d63df70b65918be608617b0
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-11-16 13:46:02 +0100

    Update TakkenTest.php

--- tests/TakkenTest.php
+++ tests/TakkenTest.php
@@ -9,20 +9,62 @@ class TakkenTest extends TestCase
 
     use DatabaseTransactions, DatabaseMigrations;
 
+    /**
+     * @group takken
+     */
     public function testKapoenenUrl()
     {
         // Front-end
         $this->visit('/takken/kapoenen');
+    }
+    
+    /**
+     * @group takken 
+     */
+    public function testWelpenUrl()
+    {
         $this->visit('/takken/welpen');
+    }
+    
+    /**
+     * @group takken
+     */
+    public function testJongGiversUrl()
+    {
         $this->visit('/takken/jong-givers');
+    }
+    
+    /**
+     * @group takken
+     */
+    public function testGiversUrl() 
+    {
         $this->visit('/takken/givers');
+    }
+    
+    /**
+     * @group takken
+     */
+    public function testJinsUrl()
+    {
         $this->visit('/takken/jins');
+    }
+    
+    /**
+     * @group takken 
+     */
+    public function testLeidingUrl() 
+    {
         $this->visit('/takken/leiding');
-
+    }
+    
+    /**
+     * @group takken
+     */
+    public function testVariousUrl()
+    {
         // Back-end
         $this->visit('/backend/takken/update');
         $this->post('/backend/takken/update');
     }
-
-
 }
