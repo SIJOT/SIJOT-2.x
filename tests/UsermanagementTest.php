4d314c9e05bd2f5e9ad222081f3b2ac3f29df4f5
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-11-16 14:09:21 +0100

    Update UsermanagementTest.php

--- tests/UsermanagementTest.php
+++ tests/UsermanagementTest.php
@@ -9,9 +9,7 @@ class UsermanagementTest extends TestCase
     use WithoutMiddleware;
 
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @group all
      */
     public function testUsermanagementRoutes()
     {
