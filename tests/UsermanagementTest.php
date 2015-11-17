44a30dfb6ac8fdf3ac5a3c4f305d83aa717638b6
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-11-17 14:30:34 +0100

    Update UsermanagementTest.php

--- tests/UsermanagementTest.php
+++ tests/UsermanagementTest.php
@@ -13,6 +13,14 @@ class UsermanagementTest extends TestCase
      */
     public function testUsermanagementRoutes()
     {
-        $this->visit('/backend/acl');
+         $users = factory(App\User::class, 3)
+           ->create()
+           ->each(function($u) {
+                $u->permission()->save(factory(App\Permission::class)->make());
+            })->load('permission');
+            
+        // $user = factory(App\User::class)->make(['id' => 5]);
+        // factory(App\Permission::class)->make(['user_id' => 5]);
+        $this->actingAs($users[0])->visit('/backend/acl');
     }
 }
