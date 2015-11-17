8211c3d6d18f2a36971305181de09aae7769fe64
Tjoosten <Tjoosten3@gmail.com>
2015-11-17 07:23:07 +0100

    meh

--- tests/authorizationTest.php
+++ tests/authorizationTest.php
@@ -6,12 +6,37 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class authorizationTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * @group all
      */
     public function testProfilePage()
     {
-        $user = factory(App\User::class)->make();
-        $this->actingAs($user)->visit('/backend/acl/profile/1');
+        $user = factory(App\User::class)->make(['id' => 5]);
+        factory(App\Permission::class)->make(['user_id' => 5]);
+        $this->actingAs($user)->visit('/backend/acl/profile/' . $user->id);
+    }
+
+    /**
+     * @group all
+     */
+    public function testBlockuserMethod()
+    {
+        $user = factory(App\User::class)->make([
+            'id' => 5
+        ]);
+
+        $this->actingAs($user)->visit('/backend/acl/block/'. $user->id);
+    }
+
+    /**
+     * @group all
+     */
+    public function testUnblockUserMethod()
+    {
+        $user = factory(App\User::class)->make([
+            'id' => 5
+        ]);
     }
 }
