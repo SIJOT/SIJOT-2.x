90cb1b624a898b6c90763780ce9d3d94303dcb06
Tjoosten <Tjoosten3@gmail.com>
2015-11-22 20:13:46 +0100

    started working on console command to add a new user

---
+++ app/Console/Commands/NewUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class NewUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'acl:new';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $choice = ['false', 'true'];
+
+        // questions
+        $name = $this->ask('What is the name of the user');
+
+        // choices
+        $verhuurPermission = $this->choice('Assign permission for the rental system.', $choice, false);
+        $loginPermission   = $this->choice('Assign permission for the user-management system.', $choice, false);
+
+
+        $this->info('The user is now registered in the system');
+    }
+}
