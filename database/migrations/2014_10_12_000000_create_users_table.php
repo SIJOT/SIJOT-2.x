2b041f7f5efca4b80f1350c859f8eaff19d9ad91
Tjoosten <Tjoosten3@gmail.com>
2015-11-23 04:07:05 +0100

    add path column for the user image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('password', 60);
             $table->integer('role');
             $table->string('blocked');
+            $table->string('avatar')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
