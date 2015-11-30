31abcfe0d574f9a7941aa19eb9634715ed12cf83
Tjoosten <Tjoosten3@gmail.com>
2015-11-30 20:01:56 +0100

    add gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,16 @@
-// Include required libraries
-var elixir = require('laravel-elixir');
-
-// Compile the .scss files.
-// Multiple file's = mix.sass(['file1.scss', 'file2.scss']);
-// Use the version file client-side = {{ elixir('css/all.css') }}
-elixir(function(mix) {
-    mix.sass('app.scss')
-        .version('js/app.js');
-});
-
-// BrowserSynch 
-// You can trigger it with the `gulp watch` command.
-elixir(function(mix) {
-    mix.browserSync();
-});
-
-// Running phpunit 
-// You can trigger it with the `gulp tdd command`
-
-
-
+// GULP DEPENDENCIES
+// ----------------------------------------------------------------------------
+var sass         = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss    = require('gulp-minify-css');
+var jshint       = require('gulp-jshint');
+var concat       = require('gulp-concat');
+var uglify       = require('gulp-uglify');
+var imageMin     = require('gulp-imagemin');
+var notify       = require('gulp-notifify');
+var rename       = require('gulp-rename');
+var cache        = require('gulp-cache');
+var del          = require('del');
 
 
 
