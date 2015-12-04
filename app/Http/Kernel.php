5af246aee33bc5a7bff44cd9e2c590b820d4a67d
Tjoosten <Tjoosten3@gmail.com>
2015-12-05 00:01:23 +0100

    dix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,8 +28,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth'       => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest'      => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'verhuurbeheer' => \App\Http\Middleware\VerhuurBeheer::class,
+        'guest'      => \App\Http\Middleware\RedirectIfAuthenticated::class,'verhuurbeheer' => \App\Http\Middleware\VerhuurBeheer::class,
 
         /*
          * Costum middleware.
