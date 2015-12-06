9c1783ea6ec753b23ff6607d039fb4b0bfcbc323
Tjoosten <Tjoosten3@gmail.com>
2015-12-06 13:32:51 +0100

    set rentalmiddleware

--- app/Http/Controllers/notificationsController.php
+++ app/Http/Controllers/notificationsController.php
@@ -17,6 +17,7 @@ class notificationsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('verhuurbeheer', ['only' => ['VerhuurAan', 'VerhuurUit']]);
     }
 
     /**
