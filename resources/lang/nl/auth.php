d2265b7becd3c1b3c3facc9e6c0b4b8bccad66c5
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-12-10 10:19:51 +0100

    [10/12/2015 - 10h19] Fix #31

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Wij konden uw login niet vinden in het systeem',
+    'throttle' => 'Teveel pogingen. Probeer op nieuw in :seconds seconden.',
+
+];
