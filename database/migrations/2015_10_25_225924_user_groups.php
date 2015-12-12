2254e98b0bf6b13418027bf2605eebd8fbc5aad2
Tjoosten <Tjoosten3@gmail.com>
2015-12-12 19:24:50 +0100

    #fix 133| Object parameters should be type hinted

--- database/migrations/2015_10_25_225924_user_groups.php
+++ database/migrations/2015_10_25_225924_user_groups.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class UserGroups extends Migration
 {
@@ -11,7 +12,7 @@ class UserGroups extends Migration
      */
     public function up()
     {
-        Schema::create('user_groups', function ($t) {
+        Schema::create('user_groups', function (Blueprint $t) {
             $t->increments('id');
             $t->string('group');
             $t->string('css_class');
