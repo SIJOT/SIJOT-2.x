cbf3d84b0e2463c08f5f21e194b20ef1d0903605
Tjoosten <Tjoosten3@gmail.com>
2015-12-15 12:31:58 +0100

    Set the table to the Groep model

--- app/Groep.php
+++ app/Groep.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Groep extends Model
 {
-    //
+    /**
+     * The table that is used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_groups';
+
+
+
 }
