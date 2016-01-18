b9f71e89f10a04f78c0f8636e4cc1708c92e7d51
Tim Joosten <Tjoosten@users.noreply.github.com>
2016-01-18 11:36:28 +0100

    Create validation.php

---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,86 @@
+<?php 
+
+  return [
+    
+    /*
+     | -----------------------------------------------------------------------------
+     | Validation Language Lines
+     | -----------------------------------------------------------------------------
+     | 
+     | The following language lines contain the default error messages used by
+     | the validator class. Some of these rules have multiple versions such 
+     | as the size rules. Feel free to tweak each of thse messages here.
+     |
+     */
+     
+     'accepted'             => '', 
+     'active_url'           => '',
+     'after'                => '', 
+     'alpha'                => '', 
+     'alpha_dash'           => '', 
+     'alpha_num'            => '', 
+     'array'                => '', 
+     'before'               => '',
+     'between'              => [
+        'numeric' => '', 
+        'file'    => '', 
+        'string'  => '', 
+        'array'   => '', 
+     ], 
+     'boolean'              => '', 
+     'confirmed'            => '', 
+     'date'                 => '', 
+     'date_format'          => '',
+     'different'            => '', 
+     'digits'               => '', 
+     'digits_between'       => '',
+     'email'                => '', 
+     'exists'               => '', 
+     'filled'               => '', 
+     'image'                => '', 
+     'in'                   => '', 
+     'integer'              => '', 
+     'ip'                   => '', 
+     'json'                 => '', 
+     'max'                  => [
+        'numeric' => '', 
+        'file'    => '', 
+        'string'  => '', 
+        'array'   => '',
+     ],
+     'mimes'                => '', 
+     'min'                  => [
+        'numeric' => '', 
+        'file'    => '', 
+        'string'  => '', 
+        'array'   => '',
+     ],
+     'not_in'               => '', 
+     'numeric'              => '', 
+     'regex'                => '', 
+     'required'             => '', 
+     'required_if'          => '', 
+     'required_with'        => '', 
+     'required_with_all'    => '', 
+     'required_without'     => '', 
+     'required_without_all' => '',
+     'same'                 => '', 
+     'size'                 => [
+        'numeric' => '', 
+        'file'    => '',
+        'string'  => '', 
+        'array'   => '',
+     ],
+     'string' => '', 
+     'timezone' => '', 
+     'unique' => '', 
+     'url' => '', 
+     
+     /*
+     | -----------------------------------------------------------------------------
+     | Costum Validation Language Lines
+     | -----------------------------------------------------------------------------
+     */ 
+     
+     
+  ]; 
