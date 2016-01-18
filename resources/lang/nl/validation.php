adb4dfa83a5206ea29ec0930370f4e83621f4d98
Tim Joosten <Tjoosten@users.noreply.github.com>
2016-01-18 13:17:10 +0100

    Update validation.php

--- resources/lang/nl/validation.php
+++ resources/lang/nl/validation.php
@@ -80,7 +80,30 @@
      | -----------------------------------------------------------------------------
      | Costum Validation Language Lines
      | -----------------------------------------------------------------------------
-     */ 
+     |
+     | Here you may specify costum validation messages for attributes using the 
+     | convention "attribute.rule" to name the lines. This makes it quick to 
+     | specify a specific custom language line for a givin attribute rule.
+     |
+     */
      
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',    
+        ],     
+    ],
+    
+    /*
+     | -----------------------------------------------------------------------------
+     | Costum Validation attributes
+     | -----------------------------------------------------------------------------
+     |
+     | The following language lines are used to swap attribute place-holders 
+     | with something more reader friendly such as E-Mail Address instead
+     | of "email". This simply helps us make messages a little cleaner.
+     |
+     */
      
+    'attributes' => [],
+    
   ]; 
