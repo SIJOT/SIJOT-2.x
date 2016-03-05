c488a30dacf27799abf0a8d83579aca39249121f
Tjoosten <Tjoosten3@gmail.com>
2016-03-05 14:00:21 +0100

    update composer

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,9 +25,5 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
         return $app;
     }
 
-    public function setUp()
-    {
-        parent::setUp();
-        Artisan::call('migrate');
-    }
+
 }
